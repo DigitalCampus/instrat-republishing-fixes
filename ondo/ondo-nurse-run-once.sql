14a8c7b77cf232681e535f2f702b5b951949bca7
Alex Little <alex@digital-campus.org>
2018-02-26 20:58:56 +0200

    add ondo nurse scripts

---
+++ ondo/ondo-nurse-run-once.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO `oppia_section` (`id`, `course_id`, `order`, `title`) VALUES (NULL, '31', '0', '{\"en\":\"Baseline\"}');
+
+INSERT INTO `oppia_activity` (`id`, `section_id`, `order`, `title`, `type`, `digest`, `baseline`, `image`, `content`, `description`) 
+VALUES (NULL, '1789', '1', '{\"en\": \"Pre-Test\"}', 'quiz', 'e81418e08ce90d1c046e6c0786cf8d5d1666cr0s2a1p5a0m0', '1', NULL, NULL, NULL);
+
