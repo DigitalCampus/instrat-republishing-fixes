b328ad17235ccdbaa914627e1f7f040edbec12ad
Alex Little <alex@digital-campus.org>
2018-02-27 12:51:35 +0200

    add bayelsa nurses

---
+++ bayelsa/bayelsa-nurse-run-once.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO `oppia_section` (`id`, `course_id`, `order`, `title`) VALUES (NULL, '2', '0', '{\"en\":\"Baseline\"}');
+
+INSERT INTO `oppia_activity` (`id`, `section_id`, `order`, `title`, `type`, `digest`, `baseline`, `image`, `content`, `description`) 
+VALUES (NULL, '309', '1', '{\"en\": \"Pre-Test\"}', 'quiz', '87add1eb69529a98eca2231d4369709c121cr0s2a1p50a0m0', '1', NULL, NULL, NULL);
+
