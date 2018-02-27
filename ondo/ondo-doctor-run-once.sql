cd4cd1b03cacac3db69449b2ab5bfdd1ebb2075e
Alex Little <alex@digital-campus.org>
2018-02-27 14:11:57 +0200

    add ondo doctor

---
+++ ondo/ondo-doctor-run-once.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO `oppia_section` (`id`, `course_id`, `order`, `title`) VALUES (NULL, '34', '0', '{\"en\":\"Baseline\"}');
+
+INSERT INTO `oppia_activity` (`id`, `section_id`, `order`, `title`, `type`, `digest`, `baseline`, `image`, `content`, `description`) 
+VALUES (NULL, '1790', '1', '{\"en\": \"Pre-Test\"}', 'quiz', '04164dee32d7703d7634743d760e4a592146cr0s2a1p5a0m0', '1', NULL, NULL, NULL);
+
