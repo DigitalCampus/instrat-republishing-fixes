400e14b26c3987bfe8a8726da92aed72be80e957
Alex Little <alex@digital-campus.org>
2018-02-28 14:21:40 +0200

    updates

--- bayelsa/bayelsa-chw-run-once.sql
+++ bayelsa/bayelsa-chw-run-once.sql
@@ -1,6 +1,6 @@
 
-INSERT INTO `oppia_section` (`id`, `course_id`, `order`, `title`) VALUES (NULL, '3', '0', '{\"en\":\"Baseline\"}');
+-- INSERT INTO `oppia_section` (`id`, `course_id`, `order`, `title`) VALUES (NULL, '3', '0', '{\"en\":\"Baseline\"}');
 
-INSERT INTO `oppia_activity` (`id`, `section_id`, `order`, `title`, `type`, `digest`, `baseline`, `image`, `content`, `description`) 
-VALUES (NULL, '308', '1', '{\"en\": \"Pre-Test\"}', 'quiz', 'c7fb2498eda3a21531c6ad2a39619cc42cr0s2a1p50a0m0', '1', NULL, NULL, NULL);
+-- INSERT INTO `oppia_activity` (`id`, `section_id`, `order`, `title`, `type`, `digest`, `baseline`, `image`, `content`, `description`) 
+-- VALUES (NULL, '308', '1', '{\"en\": \"Pre-Test\"}', 'quiz', 'c7fb2498eda3a21531c6ad2a39619cc42cr0s2a1p50a0m0', '1', NULL, NULL, NULL);
 
--- fct/fct-doctor-run-once.sql
+++ fct/fct-doctor-run-once.sql
@@ -1,5 +1,5 @@
 
-INSERT INTO `oppia_section` (`id`, `course_id`, `order`, `title`) VALUES (NULL, '25', '0', '{\"en\":\"Baseline\"}');
+-- INSERT INTO `oppia_section` (`id`, `course_id`, `order`, `title`) VALUES (NULL, '25', '0', '{\"en\":\"Baseline\"}');
 
-INSERT INTO `oppia_activity` (`id`, `section_id`, `order`, `title`, `type`, `digest`, `baseline`, `image`, `content`, `description`) VALUES (NULL, '1643', '1', '{\"en\": \"Pre-Test\"}', 'quiz', 'fd9954cf0326011cabd4b3b82ec0774e1894cr0s2a1p5a0m0', '1', NULL, NULL, NULL);
+-- INSERT INTO `oppia_activity` (`id`, `section_id`, `order`, `title`, `type`, `digest`, `baseline`, `image`, `content`, `description`) VALUES (NULL, '1643', '1', '{\"en\": \"Pre-Test\"}', 'quiz', 'fd9954cf0326011cabd4b3b82ec0774e1894cr0s2a1p5a0m0', '1', NULL, NULL, NULL);
 
